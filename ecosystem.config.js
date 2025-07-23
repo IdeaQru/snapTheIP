48760d36d23ff6888a9a874fa60ca6c24387fd73
IdeaQru <fiqru.s@gmail.com>
2025-07-24 02:48:58 +0700

    hshhshs2

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [{
+    name: 'snaptheip',
+    script: 'npx',
+    args: 'ts-node websocket-tcp-server.ts',
+    cwd: './',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production'
+    },
+    env_development: {
+      NODE_ENV: 'development'
+    },
+    log_file: './logs/combined.log',
+    out_file: './logs/out.log',
+    error_file: './logs/error.log',
+    log_date_format: 'YYYY-MM-DD HH:mm Z',
+    merge_logs: true
+  }]
+};
